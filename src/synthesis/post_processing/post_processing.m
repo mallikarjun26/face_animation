582b57616b883c03c2c9efd04914fbecba34d2fe
Mallikarjun <mallik.jeevan@gmail.com>
2015-01-08 11:07:31 +0530

    post processing

--- src/synthesis/post_processing/post_processing.m
+++ src/synthesis/post_processing/post_processing.m
@@ -6,14 +6,14 @@ function post_processing(output_path)
   videos_path = [output_path '/videos'];
 
   videos_name = dir(videos_path);
-  videos_name = videos_name(3:size(videos_name,1),:);
+  videos_name = videos_name(4:size(videos_name,1),:);
   numberOfVideos = size(videos_name,1);
   
   for i=1:numberOfVideos
     
     disp(['Processing ' int2str(i)  ' video']);
     
-    video_path = [videos_path '/' videos_name(i).name];
+    video_path = [videos_path '/' videos_name(i).name]
     video_obj  = VideoReader(video_path);
     video_mat  = read(video_obj, Inf);
     
