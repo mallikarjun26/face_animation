6cafccbcfb6005da0eb3dacfdf48a6a75e7c7558
Mallikarjun <mallik.jeevan@gmail.com>
2015-01-19 23:29:19 +0530

    first trial of video interpolation based on spline and hop distances

--- src/synthesis/post_processing/post_processing.m
+++ src/synthesis/post_processing/post_processing.m
@@ -4,9 +4,9 @@ function post_processing(output_path)
 
   % Initialization
   videos_path = [output_path '/videos'];
+  avi_videos_path = fullfile([output_path '/videos/*.avi']);
 
-  videos_name = dir(videos_path);
-  videos_name = videos_name(4:size(videos_name,1),:);
+  videos_name = dir(avi_videos_path);
   numberOfVideos = size(videos_name,1);
   
   for i=1:numberOfVideos
@@ -14,16 +14,15 @@ function post_processing(output_path)
     disp(['Processing ' int2str(i)  ' video']);
     
     video_path = [videos_path '/' videos_name(i).name]
-    video_obj  = VideoReader(video_path);
-    video_mat  = read(video_obj, Inf);
     
     %% Intensity normalization
     video_mat = intensity_normalization(video_path);
     %dump_video(video_mat, '~/Downloads/intensity_normalized.avi');
     
     %% Interpolation
-    video_mat = video_interpolation(video_mat);
-
+    interpolated_video_mat = video_interpolation(video_mat, video_path);
+    dump_video(interpolated_video_mat, '~/Downloads/interpolated_normalized.avi');
+    
     %% Face cropping
     
   end
