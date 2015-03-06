7f5a44e112f78d42ddc44453726ff9e7ee551641
Mallikarjun <mallik.jeevan@gmail.com>
2015-03-06 13:44:56 +0530

    intraface included as well

--- src/auto_best_fid_det/util/comp_models.m
+++ src/auto_best_fid_det/util/comp_models.m
@@ -6,38 +6,49 @@ function comp_models(path, list_of_faces)
     load([path '/chehra_data/intermediate_results/chehra_fids.mat']);
     load([path '/deva_data/intermediate_results/deva_fids.mat']);
     load([path '/rcpr_data/intermediate_results/rcpr_fids.mat']);
+    load([path '/intraface_data/intermediate_results/intraface_fids.mat']);
     load([path '/Faces5000/intermediate_results/facemap.mat']);
     
     %
     number_of_faces = size(list_of_faces,1);
     
     %
-    figure;
+    h = figure;
     for i=1:number_of_faces
         im = imread(facemap{1,list_of_faces(i,1)});
         clf;
         disp(num2str(list_of_faces(i,1)));
         
-        subplot(1,3,1);imshow(im);
+        subplot(1,4,1);imshow(im);
         hold on;
         if(~isempty(chehra_fids{list_of_faces(i,1)}))
             plot_chehra_fids(chehra_fids{list_of_faces(i,1)}, im, 0, 1);
         end
         title('Chehra fids');
         
-        subplot(1,3,2);imshow(im);
+        subplot(1,4,2);imshow(im);
         hold on;
         if(~isempty(deva_fids{list_of_faces(i,1)}))
             plot_deva_fids(deva_fids{list_of_faces(i,1)}, im, 0, 1);
         end
         title('Deva fids');
         
-        subplot(1,3,3);imshow(im);
+        subplot(1,4,3);imshow(im);
+        hold on;
+        if(~isempty(intraface_fids{list_of_faces(i,1)}))
+            plot_intraface_fids(intraface_fids{list_of_faces(i,1)}, im, 0, 1);
+        end
+        title('Intraface fids');
+
+        subplot(1,4,4);imshow(im);
         hold on;
         if(~isempty(rcpr_fids{list_of_faces(i,1)}))
             plot_rcpr_fids(rcpr_fids{list_of_faces(i,1)}, im, 0, 1);
         end
         title('RCPR fids');
+        
+        %saveas(h, ['/home/mallikarjun/data/results/26_feb/452_dec_th_intraface/' num2str(list_of_faces(i,1)) '.jpg']);
+        %saveas(h, ['/home/mallikarjun/Desktop/dump/' num2str(list_of_faces(i,1)) '.jpg']);
         
         pause;
     end
