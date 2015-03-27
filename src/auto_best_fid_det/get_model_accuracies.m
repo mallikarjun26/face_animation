eee66bf1011fc74becd68d08aebd579937ae3123
Mallikarjun <mallik.jeevan@gmail.com>
2015-03-27 20:39:05 +0530

    deva bug. dist shape for only non nan fids.

--- src/auto_best_fid_det/get_model_accuracies.m
+++ src/auto_best_fid_det/get_model_accuracies.m
@@ -1,4 +1,4 @@
-function [chehra_accuracy, deva_accuracy, intraface_accuracy, rcpr_accuracy, our_model_accuracy] = get_model_accuracies(path, dataset, per_part_error_threshold)
+function [chehra_accuracy, deva_accuracy, intraface_accuracy, rcpr_accuracy, our_model_accuracy] = get_model_accuracies(path, face_list, dataset, per_part_error_threshold, auto_train_select)
 
     %
     if(dataset == 'jack')
@@ -8,26 +8,19 @@ function [chehra_accuracy, deva_accuracy, intraface_accuracy, rcpr_accuracy, our
         load([path '/intraface_data/intermediate_results/intraface_fids.mat']);
         load([path '/rcpr_data/intermediate_results/rcpr_fids.mat']);
         load([path '/Faces5000/intermediate_results/facemap.mat']);
-    elseif(dataset == 'lfpw')
-        load([path '/common_data/fids_mapping/chehra_deva_intraface_rcpr_common_fids.mat']);
-        load([path '/lfpw_data/chehra_fids.mat']);
-        load([path '/lfpw_data/deva_fids.mat']);
-        load([path '/lfpw_data/intraface_fids.mat']);
-        load([path '/lfpw_data/rcpr_fids.mat']);
-        load([path '/lfpw_data/facemap.mat']);
-        load([path '/lfpw_data/ground_truth.mat']);
     else
         load([path '/common_data/fids_mapping/chehra_deva_intraface_rcpr_common_fids.mat']);
-        load([path '/cofw_data/chehra_fids.mat']);
-        load([path '/cofw_data/deva_fids.mat']);
-        load([path '/cofw_data/intraface_fids.mat']);
-        load([path '/cofw_data/rcpr_fids.mat']);
-        load([path '/cofw_data/facemap.mat']);
-        load([path '/cofw_data/ground_truth.mat']);
+        load([path '/' dataset '_data/chehra_fids.mat']);
+        load([path '/' dataset '_data/deva_fids.mat']);
+        load([path '/' dataset '_data/intraface_fids.mat']);
+        load([path '/' dataset '_data/rcpr_fids.mat']);
+        load([path '/' dataset '_data/ground_truth.mat']);
+        load([path '/' dataset '_data/facemap.mat']);
+        load([path '/' dataset '_data/ground_truth.mat']);
     end
     
     %
-    number_of_faces = size(facemap,2);
+    number_of_faces = size(face_list,1);
     chehra_model_results = zeros(number_of_faces, 1);
     deva_model_results = zeros(number_of_faces, 1);
     intraface_model_results = zeros(number_of_faces, 1);
@@ -35,12 +28,13 @@ function [chehra_accuracy, deva_accuracy, intraface_accuracy, rcpr_accuracy, our
 
     %
     for i=1:number_of_faces
-    
-        s_chehra_fid    = chehra_fids{i};
-        s_deva_fid      = deva_fids{i};
-        s_rcpr_fid      = rcpr_fids{i};
-        s_intraface_fid      = intraface_fids{i};
-        s_ground_truth_fid   = ground_truth{i}; 
+   
+        index = face_list(i);
+        s_chehra_fid    = chehra_fids{index};
+        s_deva_fid      = deva_fids{index};
+        s_rcpr_fid      = rcpr_fids{index};
+        s_intraface_fid      = intraface_fids{index};
+        s_ground_truth_fid   = ground_truth{index}; 
 
         %
         if(isempty(s_deva_fid))
@@ -64,22 +58,24 @@ function [chehra_accuracy, deva_accuracy, intraface_accuracy, rcpr_accuracy, our
         [s_rcpr_fid] = get_fid(s_rcpr_fid, 4, chehra_deva_intraface_rcpr_common_fids);
         if(dataset == 'lfpw')
             [s_ground_truth_fid] = get_fid(s_ground_truth_fid, 7, chehra_deva_intraface_rcpr_common_fids);
-        else
+        elseif(dataset == 'cofw')
             [s_ground_truth_fid] = get_fid(s_ground_truth_fid, 8, chehra_deva_intraface_rcpr_common_fids);
+        elseif(dataset == 'aflw')
+            [s_ground_truth_fid] = get_fid(s_ground_truth_fid, 9, chehra_deva_intraface_rcpr_common_fids);
         end
 
         %
-        [chehra_model_results(i) deva_model_results(i) intraface_model_results(i) rcpr_model_results(i)] = check_accuracy(s_chehra_fid, s_deva_fid, s_intraface_fid, s_rcpr_fid, s_ground_truth_fid, per_part_error_threshold); 
+        [chehra_model_results(i) deva_model_results(i) intraface_model_results(i) rcpr_model_results(i)] = check_accuracy(s_chehra_fid, s_deva_fid, s_intraface_fid, s_rcpr_fid, s_ground_truth_fid, per_part_error_threshold, index); 
     end
 
-    selected_models = get_best_model(path, dataset);
+    selected_models = get_best_model(path, dataset, auto_train_select);
 
-    [chehra_accuracy, deva_accuracy, intraface_accuracy, rcpr_accuracy, our_model_accuracy] = get_percentages(chehra_model_results, deva_model_results, intraface_model_results, rcpr_model_results, selected_models); 
+    [chehra_accuracy, deva_accuracy, intraface_accuracy, rcpr_accuracy, our_model_accuracy] = get_percentages(chehra_model_results, deva_model_results, intraface_model_results, rcpr_model_results, selected_models, face_list); 
 
 
 end
 
-function[chehra_accuracy, deva_accuracy, intraface_accuracy, rcpr_accuracy, our_model_accuracy] = get_percentages(chehra_model_results, deva_model_results, intraface_model_results, rcpr_model_results, selected_models)
+function[chehra_accuracy, deva_accuracy, intraface_accuracy, rcpr_accuracy, our_model_accuracy] = get_percentages(chehra_model_results, deva_model_results, intraface_model_results, rcpr_model_results, selected_models, face_list)
     
     %
     number_of_faces = size(chehra_model_results, 1);
@@ -93,7 +89,9 @@ function[chehra_accuracy, deva_accuracy, intraface_accuracy, rcpr_accuracy, our_
     our_model_accuracy = 0;
     for i=1:number_of_faces
 
-        switch selected_models(i)
+        index = face_list(i);
+
+        switch selected_models(index)
 
             case 1
                 if(chehra_model_results(i)==1)
@@ -117,21 +115,39 @@ function[chehra_accuracy, deva_accuracy, intraface_accuracy, rcpr_accuracy, our_
 
 end
 
-function [chehra_model_results deva_model_results intraface_model_results rcpr_model_results] = check_accuracy(s_chehra_fid, s_deva_fid, s_intraface_fid, s_rcpr_fid, s_ground_truth_fid, per_part_error_threshold)
+function shape_dist = get_shape_dist(fid_1, fid_2)
+    
+    non_nan_1 = find(~isnan(fid_1(:,1)));
+    non_nan_2 = find(~isnan(fid_2(:,1)));
+    non_nan = intersect(non_nan_1, non_nan_2);
+
+    shape_1 = [fid_1(non_nan,1)' fid_1(non_nan,2)'];
+    shape_2 = [fid_2(non_nan,1)' fid_2(non_nan,2)'];
+
+    shape_dist = pdist([shape_1; shape_2]);
+    shape_dist = shape_dist / size(non_nan,1);
+end
+
+function [chehra_model_results deva_model_results intraface_model_results rcpr_model_results] = check_accuracy(s_chehra_fid, s_deva_fid, s_intraface_fid, s_rcpr_fid, s_ground_truth_fid, per_part_error_threshold, index)
 
     number_of_parts = size(s_ground_truth_fid,1);
 
-    t_gt = double([s_ground_truth_fid(:,1)' s_ground_truth_fid(:,2)']); 
-    t_1 =  double([s_chehra_fid(:,1)' s_chehra_fid(:,2)']); 
-    t_2 =  double([s_deva_fid(:,1)' s_deva_fid(:,2)']); 
-    t_3 =  double([s_intraface_fid(:,1)' s_intraface_fid(:,2)']); 
-    t_4 =  double([s_rcpr_fid(:,1)' s_rcpr_fid(:,2)']); 
+    % t_gt = double([s_ground_truth_fid(:,1)' s_ground_truth_fid(:,2)']); 
+    % t_1 =  double([s_chehra_fid(:,1)' s_chehra_fid(:,2)']); 
+    % t_2 =  double([s_deva_fid(:,1)' s_deva_fid(:,2)']); 
+    % t_3 =  double([s_intraface_fid(:,1)' s_intraface_fid(:,2)']); 
+    % t_4 =  double([s_rcpr_fid(:,1)' s_rcpr_fid(:,2)']); 
 
-    chehra_shape_dist = pdist([t_gt; t_1]) / number_of_parts;
-    deva_shape_dist = pdist([t_gt; t_2]) / number_of_parts;
-    intraface_shape_dist = pdist([t_gt; t_3]) / number_of_parts;
-    rcpr_shape_dist = pdist([t_gt; t_4]) / number_of_parts;
+    % chehra_shape_dist = pdist([t_gt; t_1]) / number_of_parts;
+    % deva_shape_dist = pdist([t_gt; t_2]) / number_of_parts;
+    % intraface_shape_dist = pdist([t_gt; t_3]) / number_of_parts;
+    % rcpr_shape_dist = pdist([t_gt; t_4]) / number_of_parts;
     
+    chehra_shape_dist = get_shape_dist(s_chehra_fid, s_ground_truth_fid);
+    deva_shape_dist = get_shape_dist(s_deva_fid, s_ground_truth_fid);
+    intraface_shape_dist = get_shape_dist(s_intraface_fid, s_ground_truth_fid);
+    rcpr_shape_dist = get_shape_dist(s_rcpr_fid, s_ground_truth_fid);
+
     if(chehra_shape_dist < per_part_error_threshold)
         chehra_model_results = 1;
     else
@@ -155,7 +171,9 @@ function [chehra_model_results deva_model_results intraface_model_results rcpr_m
     else
         rcpr_model_results = 0;
     end
-
+    
+%     addpath ../util/;
+%     comp_models('~/data/face_animation', [index], 'aflw', 1);
 end
 
 
@@ -167,9 +185,9 @@ function [fid_o] = get_fid(fid, fid_mode, chehra_deva_intraface_rcpr_common_fids
         if( (fid.c>3) && (fid.c<11) )
             fid_mode_u = 2;
         elseif(fid.c>10)
-            fid_mode_u = 5;
-        else
             fid_mode_u = 6;
+        else
+            fid_mode_u = 5;
         end
         
         
@@ -183,11 +201,20 @@ function [fid_o] = get_fid(fid, fid_mode, chehra_deva_intraface_rcpr_common_fids
             end
         end
         
-        fid_o = uint32([y x]);
+        fid_o = ([y x]);
     else
-        y = fid(chehra_deva_intraface_rcpr_common_fids(:,fid_mode),1);
-        x = fid(chehra_deva_intraface_rcpr_common_fids(:,fid_mode),2);
-        fid_o = uint32([y x]);
+        y = NaN * ones(size(chehra_deva_intraface_rcpr_common_fids,1), 1);
+        x = y;
+        for i=1:size(chehra_deva_intraface_rcpr_common_fids,1)
+            index = chehra_deva_intraface_rcpr_common_fids(i, fid_mode);
+            if(~isnan(index))
+                y(i,1) = fid(index, 1);
+                x(i,1) = fid(index, 2);
+            end
+        end
+        
+        fid_o = ([y x]);
+
     end
 
 end
