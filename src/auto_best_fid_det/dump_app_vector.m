c10d64c6f298ed57a92179181f2fc056324d3816
Mallikarjun <mallik.jeevan@gmail.com>
2015-03-27 20:29:08 +0530

    ground truth app vector addition. deva extreme bug fix.

--- src/auto_best_fid_det/dump_app_vector.m
+++ src/auto_best_fid_det/dump_app_vector.m
@@ -5,6 +5,14 @@ function dump_app_vector(path, dataset)
     run('/home/mallikarjun/src/vlfeat/vlfeat-0.9.19/toolbox/vl_setup')
 
     %
+    chehra_fids = [];
+    deva_fids = [];
+    rcpr_fids = [];
+    intraface_fids = [];
+    ground_truth = [];
+    chehra_deva_intraface_rcpr_common_fids = [];
+    
+    %
     if(dataset == 'jack')
         load([path '/common_data/fids_mapping/chehra_deva_intraface_rcpr_common_fids.mat']);
         load([path '/chehra_data/intermediate_results/chehra_fids.mat']);
@@ -12,20 +20,14 @@ function dump_app_vector(path, dataset)
         load([path '/intraface_data/intermediate_results/intraface_fids.mat']);
         load([path '/rcpr_data/intermediate_results/rcpr_fids.mat']);
         load([path '/Faces5000/intermediate_results/facemap.mat']);
-    elseif(dataset == 'lfpw')
-        load([path '/common_data/fids_mapping/chehra_deva_intraface_rcpr_common_fids.mat']);
-        load([path '/lfpw_data/chehra_fids.mat']);
-        load([path '/lfpw_data/deva_fids.mat']);
-        load([path '/lfpw_data/intraface_fids.mat']);
-        load([path '/lfpw_data/rcpr_fids.mat']);
-        load([path '/lfpw_data/facemap.mat']);
     else
         load([path '/common_data/fids_mapping/chehra_deva_intraface_rcpr_common_fids.mat']);
-        load([path '/cofw_data/chehra_fids.mat']);
-        load([path '/cofw_data/deva_fids.mat']);
-        load([path '/cofw_data/intraface_fids.mat']);
-        load([path '/cofw_data/rcpr_fids.mat']);
-        load([path '/cofw_data/facemap.mat']);
+        load([path '/' dataset '_data/chehra_fids.mat']);
+        load([path '/' dataset '_data/deva_fids.mat']);
+        load([path '/' dataset '_data/intraface_fids.mat']);
+        load([path '/' dataset '_data/rcpr_fids.mat']);
+        load([path '/' dataset '_data/ground_truth.mat']);
+        load([path '/' dataset '_data/facemap.mat']);
     end
 
     %
@@ -35,11 +37,11 @@ function dump_app_vector(path, dataset)
     deva_app_vector = cell(number_of_faces, 1);
     intraface_app_vector = cell(number_of_faces, 1);
     rcpr_app_vector = cell(number_of_faces, 1);
+    ground_truth_app_vector = cell(number_of_faces, 1);
 
     %
     for i=1:number_of_faces
-        
-        if(mod(i,10) == 0)
+        if(mod(i,1) == 0)
             disp([ num2str(i) '/' num2str(number_of_faces) 'done' ]);
         end
            
@@ -54,53 +56,89 @@ function dump_app_vector(path, dataset)
         s_deva_fid      = deva_fids{i};
         s_rcpr_fid      = rcpr_fids{i};
         s_intraface_fid      = intraface_fids{i};
+        s_ground_truth_fid   = ground_truth{i};
          
         if(isempty(s_deva_fid))
-            s_deva_fid.xy = ones(68,4);
+            s_deva_fid.xy = NaN * ones(68,4);
             s_deva_fid.c = 7;
         end
         if(isempty(s_intraface_fid))
-            s_intraface_fid = ones(49,2);
+            s_intraface_fid = NaN * ones(49,2);
         end
         if(isempty(s_rcpr_fid))
-            s_rcpr_fid = ones(29,2);
+            s_rcpr_fid = NaN * ones(29,2);
         end
         if(isempty(s_chehra_fid))
-            s_chehra_fid = ones(49,2);
+            s_chehra_fid = NaN * ones(49,2);
         end
 
         [fid_1] = get_fid(s_chehra_fid, 1, chehra_deva_intraface_rcpr_common_fids);
         [fid_2] = get_fid(s_deva_fid, 2, chehra_deva_intraface_rcpr_common_fids);
         [fid_3] = get_fid(s_intraface_fid, 3, chehra_deva_intraface_rcpr_common_fids);
         [fid_4] = get_fid(s_rcpr_fid, 4, chehra_deva_intraface_rcpr_common_fids);
+        if(dataset == 'lfpw')
+            [fid_5] = get_fid(s_ground_truth_fid, 7, chehra_deva_intraface_rcpr_common_fids);
+        elseif(dataset == 'cofw')
+            [fid_5] = get_fid(s_ground_truth_fid, 8, chehra_deva_intraface_rcpr_common_fids);
+        elseif(dataset == 'aflw')
+            [fid_5] = get_fid(s_ground_truth_fid, 9, chehra_deva_intraface_rcpr_common_fids);
+        end
     
         chehra_part_app_vector = cell(number_of_parts,1);
         deva_part_app_vector = cell(number_of_parts,1);
         intraface_part_app_vector = cell(number_of_parts,1);
         rcpr_part_app_vector = cell(number_of_parts,1);
-
+        ground_truth_part_app_vector = cell(number_of_parts,1);
+        
+        fea_vec_size = 535;
         for j=1:number_of_parts
             y = fid_1(j,1); 
             x = fid_1(j,2); 
-            chehra_part_app_vector{j} = get_app_feature(x, y, im);
+            
+            if(isnan(y) || isnan(x))
+                chehra_part_app_vector{j} = NaN * ones(1, fea_vec_size);
+            else
+                chehra_part_app_vector{j} = get_app_feature(x, y, im);
+            end
 
             y = fid_2(j,1); 
             x = fid_2(j,2); 
-            deva_part_app_vector{j} = get_app_feature(x, y, im);
+            if(isnan(y) || isnan(x))
+                deva_part_app_vector{j} = NaN * ones(1, fea_vec_size);
+            else
+                deva_part_app_vector{j} = get_app_feature(x, y, im);
+            end
 
             y = fid_3(j,1); 
             x = fid_3(j,2); 
-            intraface_part_app_vector{j} = get_app_feature(x, y, im);
+            if(isnan(y) || isnan(x))
+                intraface_part_app_vector{j} = NaN * ones(1, fea_vec_size);
+            else
+                intraface_part_app_vector{j} = get_app_feature(x, y, im);
+            end
 
             y = fid_4(j,1); 
             x = fid_4(j,2); 
-            rcpr_part_app_vector{j} = get_app_feature(x, y, im);
+            if(isnan(y) || isnan(x))
+                rcpr_part_app_vector{j} = NaN * ones(1, fea_vec_size);
+            else
+                rcpr_part_app_vector{j} = get_app_feature(x, y, im);
+            end
+
+            y = fid_5(j,1); 
+            x = fid_5(j,2); 
+            if(isnan(y) || isnan(x))
+                ground_truth_part_app_vector{j} = NaN * ones(1, fea_vec_size);
+            else
+                ground_truth_part_app_vector{j} = get_app_feature(x, y, im);
+            end
         end
 
         chehra_app_vector{i} = chehra_part_app_vector;
         deva_app_vector{i} = deva_part_app_vector;
         intraface_app_vector{i} = intraface_part_app_vector;
         rcpr_app_vector{i} = rcpr_part_app_vector;
+        ground_truth_app_vector{i} = ground_truth_part_app_vector;
     end
 
     %
@@ -108,13 +146,16 @@ function dump_app_vector(path, dataset)
     save([path '/' dataset '_data/app_based_results/deva_app_vector.mat'], 'deva_app_vector');
     save([path '/' dataset '_data/app_based_results/intraface_app_vector.mat'], 'intraface_app_vector');
     save([path '/' dataset '_data/app_based_results/rcpr_app_vector.mat'], 'rcpr_app_vector');
+    save([path '/' dataset '_data/app_based_results/ground_truth_app_vector.mat'], 'ground_truth_app_vector');
 
 end
 
 function part_app_feat = get_app_feature(x, y, im)
 
     hog_feat = get_hog_feature(x, y, im);
+    
     sift_feat = get_sift_feature(x, y, im);
+    
     part_app_feat = [hog_feat sift_feat];
 end
 
@@ -150,15 +191,15 @@ end
 
 function [fid_o] = get_fid(fid, fid_mode, chehra_deva_intraface_rcpr_common_fids)
 
-    if((fid_mode == 2) || (fid_mode == 5) || (fid_mode == 6) )
+    if(fid_mode == 2)
         
         fid_mode_u = 0;
         if( (fid.c>3) && (fid.c<11) )
             fid_mode_u = 2;
         elseif(fid.c>10)
-            fid_mode_u = 5;
-        else
             fid_mode_u = 6;
+        else
+            fid_mode_u = 5;
         end
         
         
@@ -172,11 +213,24 @@ function [fid_o] = get_fid(fid, fid_mode, chehra_deva_intraface_rcpr_common_fids
             end
         end
         
-        fid_o = uint32([y x]);
+        fid_o = ([y x]);
     else
-        y = fid(chehra_deva_intraface_rcpr_common_fids(:,fid_mode),1);
-        x = fid(chehra_deva_intraface_rcpr_common_fids(:,fid_mode),2);
-        fid_o = uint32([y x]);
+        number_of_parts = size(chehra_deva_intraface_rcpr_common_fids, 1);
+        
+        y = NaN * ones(size(chehra_deva_intraface_rcpr_common_fids,1), 1);
+        x = y;
+        for i=1:number_of_parts
+            index = chehra_deva_intraface_rcpr_common_fids(i,fid_mode);
+            if(~isnan(index))
+                y(i,1) = fid(index, 1);
+                x(i,1) = fid(index, 2);
+            end
+        end
+        fid_o = ([y x]);
+        
+%         y = fid(chehra_deva_intraface_rcpr_common_fids(:,fid_mode),1);
+%         x = fid(chehra_deva_intraface_rcpr_common_fids(:,fid_mode),2);
+%         fid_o = ([y x]);
     end
 
 end
