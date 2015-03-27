08021ddef4b689f3452d4763f06ad2cc60dcb1b9
Mallikarjun <mallik.jeevan@gmail.com>
2015-03-27 20:41:28 +0530

    aflw ground truth

--- src/auto_best_fid_det/util/find_chehra_deva_intraface_rcpr_common_fids.m
+++ src/auto_best_fid_det/util/find_chehra_deva_intraface_rcpr_common_fids.m
@@ -21,8 +21,10 @@ function find_chehra_deva_intraface_rcpr_common_fids(path)
     common_lfpw = intersect(common_lfpw, rcpr_lfpw{1,2});
     common_lfpw = intersect(common_lfpw, intraface_lfpw{1,2});
 
-    chehra_deva_intraface_rcpr_common_fids = zeros(size(common_lfpw,1),8);
+    chehra_deva_intraface_rcpr_common_fids = zeros(size(common_lfpw,1), 10);
     cofw_ground_truth = [ 1 5 3 4 7 2 21 19 22 20 9 11 12 10 23 25 24 28 26 27 ]';
+    aflw_ground_truth = [ 1 2 3 4 5 6 15 14 NaN 16 7 9 10 12 18 NaN 20 NaN NaN NaN ]';
+    aflw_ground_truth_com = [ 1 2 3 4 5 6 15 14 44 16 7 9 10 12 18 56 20 61 19 70 ]';
     
     for i=1:size(common_lfpw,1)
 
@@ -46,11 +48,13 @@ function find_chehra_deva_intraface_rcpr_common_fids(path)
             ind_dr = deva_ex_right_lfpw{1,1}(ind_dr,1);
         end
         
-        chehra_deva_intraface_rcpr_common_fids(i,:) = [double(chehra_lfpw{1,1}(ind_c,1)) double(deva_lfpw{1,1}(ind_d,1)) double(intraface_lfpw{1,1}(ind_i,1)) double(rcpr_lfpw{1,1}(ind_r,1)) double(ind_dl) double(ind_dr) double(par_common_lfpw) double(0)];
+        chehra_deva_intraface_rcpr_common_fids(i,:) = [double(chehra_lfpw{1,1}(ind_c,1)) double(deva_lfpw{1,1}(ind_d,1)) double(intraface_lfpw{1,1}(ind_i,1)) double(rcpr_lfpw{1,1}(ind_r,1)) double(ind_dl) double(ind_dr) double(par_common_lfpw) double(0) double(0) double(0)];
         
     end
     
     chehra_deva_intraface_rcpr_common_fids(:,8) = cofw_ground_truth; 
+    chehra_deva_intraface_rcpr_common_fids(:,9) = aflw_ground_truth; 
+    chehra_deva_intraface_rcpr_common_fids(:,10) = aflw_ground_truth_com; 
     save([path '/common_data/fids_mapping/chehra_deva_intraface_rcpr_common_fids.mat'], 'chehra_deva_intraface_rcpr_common_fids');
     
 end
