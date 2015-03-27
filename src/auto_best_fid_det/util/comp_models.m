5744a1bc56383a0070e0eafc1771186d630c17e0
Mallikarjun <mallik.jeevan@gmail.com>
2015-03-27 20:40:30 +0530

    dataset based. ground truth

--- src/auto_best_fid_det/util/comp_models.m
+++ src/auto_best_fid_det/util/comp_models.m
@@ -9,30 +9,20 @@ function comp_models(path, list_of_faces, dataset, awesome_enable)
         load([path '/rcpr_data/intermediate_results/rcpr_fids.mat']);
         load([path '/intraface_data/intermediate_results/intraface_fids.mat']);
         load([path '/Faces5000/intermediate_results/facemap.mat']);
-    else if(dataset == 'lfpw')
-        load([path '/lfpw_data/chehra_fids.mat']);
-        load([path '/lfpw_data/deva_fids.mat']);
-        load([path '/lfpw_data/rcpr_fids.mat']);
-        load([path '/lfpw_data/intraface_fids.mat']);
-        load([path '/lfpw_data/facemap.mat']);
-        if(awesome_enable)
-            load([path '/lfpw_data/app_based_results/selected_models.mat'])
-        else
-            selected_models = zeros(size(list_of_faces,1),1); 
-        end
     else
-        load([path '/cofw_data/chehra_fids.mat']);
-        load([path '/cofw_data/deva_fids.mat']);
-        load([path '/cofw_data/rcpr_fids.mat']);
-        load([path '/cofw_data/intraface_fids.mat']);
-        load([path '/cofw_data/facemap.mat']);
+        load([path '/' dataset '_data/chehra_fids.mat']);
+        load([path '/' dataset '_data/deva_fids.mat']);
+        load([path '/' dataset '_data/rcpr_fids.mat']);
+        load([path '/' dataset '_data/intraface_fids.mat']);
+        load([path '/' dataset '_data/ground_truth.mat']);
+        load([path '/' dataset '_data/facemap.mat']);
         if(awesome_enable)
-            load([path '/cofw_data/app_based_results/selected_models.mat'])
+            load([path '/' dataset '_data/app_based_results/selected_models.mat'])
         else
-            selected_models = zeros(size(list_of_faces,1),1); 
+            selected_models = zeros(size(facemap,2),1); 
         end
     end
-
+    
     %
     number_of_faces = size(list_of_faces,1);
     
@@ -43,7 +33,7 @@ function comp_models(path, list_of_faces, dataset, awesome_enable)
         clf;
         disp(num2str(list_of_faces(i,1)));
         
-        subplot(1,4,1);imshow(im);
+        subplot(1,5,1);imshow(im);
         hold on;
         if(~isempty(chehra_fids{list_of_faces(i,1)}))
             if(selected_models(list_of_faces(i,1))==1)
@@ -54,7 +44,7 @@ function comp_models(path, list_of_faces, dataset, awesome_enable)
         end
         title('Chehra fids');
         
-        subplot(1,4,2);imshow(im);
+        subplot(1,5,2);imshow(im);
         hold on;
         if(~isempty(deva_fids{list_of_faces(i,1)}))
             if(selected_models(list_of_faces(i,1))==2)
@@ -65,7 +55,7 @@ function comp_models(path, list_of_faces, dataset, awesome_enable)
         end
         title('Deva fids');
         
-        subplot(1,4,3);imshow(im);
+        subplot(1,5,3);imshow(im);
         hold on;
         if(~isempty(intraface_fids{list_of_faces(i,1)}))
             if(selected_models(list_of_faces(i,1))==3)
@@ -76,7 +66,7 @@ function comp_models(path, list_of_faces, dataset, awesome_enable)
         end
         title('Intraface fids');
 
-        subplot(1,4,4);imshow(im);
+        subplot(1,5,4);imshow(im);
         hold on;
         if(~isempty(rcpr_fids{list_of_faces(i,1)}))
             if(selected_models(list_of_faces(i,1))==4)
@@ -87,6 +77,13 @@ function comp_models(path, list_of_faces, dataset, awesome_enable)
         end
         title('RCPR fids');
         
+        subplot(1,5,5);imshow(im);
+        hold on;
+        if(~isempty(ground_truth{list_of_faces(i,1)}))
+            plot_intraface_fids(ground_truth{list_of_faces(i,1)}, im, 0, 1, 'r.');
+        end
+        title('Ground truth fids');
+        %pause;
         %saveas(h, ['/home/mallikarjun/data/results/26_feb/452_dec_th_intraface/' num2str(list_of_faces(i,1)) '.jpg']);
         saveas(h, [path '/' dataset '_data/visual_results/' num2str(i) '.jpg']);
         
